9696a583c9dd169703a01313d42d6887c6cf7fb4
Diégo Stéphan Jeandon Rodríguez <diego.jeandon@gmail.com>
2021-04-25 21:36:00 +0200

    :pencil: FTP Zones calculator

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/**
- * Returns the Theorical Maximum Heart Rate
- * @param {Number} age 
- * @param {'m','M','f','F'} gender 
- * @returns Number
- */
 exports.TheoricalMaxHeartRate = (age, gender) => {
     switch (String(gender).toLocaleLowerCase()) {
         case 'm':
@@ -13,4 +7,18 @@ exports.TheoricalMaxHeartRate = (age, gender) => {
         default:
             return 223 - age
     }
+}
+
+exports.FTPZones = (age, gender) => {
+    let maxHeartRate = this.TheoricalMaxHeartRate(age, gender)
+    let zones = {
+        "zone1": (maxHeartRate * 0.75).toFixed(0),
+        "zone2": (maxHeartRate * 0.85).toFixed(0),
+        "zone3": (maxHeartRate * 0.92).toFixed(0),
+        "zone4": (maxHeartRate * 0.96).toFixed(0),
+        "zone5": (maxHeartRate * 1.05).toFixed(0),
+        "zone6": (maxHeartRate * 1.2).toFixed(0),
+        "zone7": (maxHeartRate * 1.5).toFixed(0),
+    }
+    return zones
 }
