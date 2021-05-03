afed39d33cd7fbd26c73e6fc4d5942257e8d8c3a
Diégo Stéphan Jeandon Rodríguez <diego.jeandon@gmail.com>
2021-05-03 09:57:36 +0200

    :rocket: Methods Update

--- README.md
+++ README.md
@@ -1,71 +1,75 @@
-# FTPModule
+# fthrmodule
 ***
 Module designed for the calculation of cardiac parameters and to have the minimum of dependencies with other modules and to be as fast and light as possible.
 
 | Function  |  Parameters |  Return |
 |---|---|---|
 | `TheoricalMaxHeartRate`  | `age: Number` `gender: {'m', 'f'}`  | `Number`  |
-|  `FTPZones` | `age: Number` `gender: {'m', 'f'}`  | `Object: {"zone1": Number,"zone2": Number,"zone3": Number,"zone4": Number,"zone5": Number,"zone6": Number,"zone7": Number,}`  |
-| `HeartRateExtractor`  | `file: GPX file from Strava`  | `Array: ['Number', 'Number', 'Number', 'Number', 'Number', 'Number', ...]`  |
-| `FTPZone`  | `age: Number` `gender: {'m', 'f'}` `heartRate: Number`  | `String: {zone1, zone2, zone3, zone4, zone5, zone6, zone7}`  |
-| `FTPZonesPercentage`  | `age: Number` `gender: {'m', 'f'}` `arr: arr: Array of Numbers from HeartRateExtractor function` | `Object: { z1: Percentage, z2: Percentage, z3: Percentage, z4: Percentage, z5: Percentage, z6: Percentage, z7: Percentage }`  |
+|  `FTHRZones` | `age: Number` `gender: {'m', 'f'}` `sport: {'c', 'r'}`  | `Object: {"zone1": Number,"zone2": Number,"zone3": Number,"zone4": Number,"zone5a": Number,"zone5b": Number,"zone5c": Number,}`  |
+| `HeartRateExtractor`  | `file: GPX file from Strava`  | `Array: ['Number', 'Number', 'Number', 'Number', 'Number', 'Number']`  |
+| `FTHRZone`  | `age: Number` `gender: {'m', 'f'}` `heartRate: Number` `sport: {'c', 'r'}`  | `String: {zone1, zone2, zone3, zone4, zone5a, zone5b, zone5c}`  |
+| `FTHRZonesPercentage`  | `age: Number` `gender: {'m', 'f'}` `sport: {'c', 'r'}` `arr: Array of Numbers from HeartRateExtractor function` | `Object: { z1: Percentage, z2: Percentage, z3: Percentage, z4: Percentage, z5a: Percentage, z5b: Percentage, z5c: Percentage }`  |
 
 ***
 ## Instalation
 
-- NPM: `npm i ftpmodule`
-- Yarn: `yarn add ftpmodule`
+- NPM: `npm i fthrmodule`
+- Yarn: `yarn add fthrmodule`
 ***
 
 ## How to Use
 - `TheoricalMaxHeartRate`:
 ```javascript
-const { TheoricalMaxHeartRate } = require('ftpmodule')
+const { TheoricalMaxHeartRate } = require('fthrmodule')
 console.log(TheoricalMaxHeartRate(23, 'm'))
 // Should return 197
 ```
-- `FTPZones`:
+- `FTHRZones`:
 ```javascript
-const { FTPZones } = require('ftpmodule')
-console.log(FTPZones(23, 'm'))
+const { FTHRZones } = require('fthrmodule')
+console.log(FTHRZones(23, 'm'))
 /* Should return {
         zone1: '148',
         zone2: '167',
         zone3: '181',
         zone4: '189',
-        zone5: '207',
-        zone6: '236',
-        zone7: '296'
+        zone5a: '207',
+        zone5b: '236',
+        zone5c: '296'
     } */
 ```
 - `HeartRateExtractor`:
 ```javascript
 const fs = require('fs')
-const { HeartRateExtractor } = require('ftpmodule')
+const { HeartRateExtractor } = require('fthrmodule')
 const data = fs.readFileSync('./file.gpx', 'UTF-8');
 console.log(HeartRateExtractor(data))
 // Should return a JSON array of number 
 ```
-- `FTPZone`:
+- `FTHRZone`:
 ```javascript
-const { FTPZone } = require('ftpmodule')
-console.log(FTPZone(23, 'm', 184))
+const { FTHRZone } = require('fthrmodule')
+console.log(FTHRZone(23, 'm', 184))
 // Should return zone4
 ```
-- `FTPZonesPercentage`:
+- `FTHRZonesPercentage`:
 ```javascript
 const fs = require('fs')
-const { FTPZonesPercentage, HeartRateExtractor } = require('ftpmodule')
+const { FTHRZonesPercentage, HeartRateExtractor } = require('fthrmodule')
 const data = fs.readFileSync('./file.gpx', 'UTF-8')
-console.log(FTPZonesPercentage(23, 'm', HeartRateExtractor(data)))
+console.log(FTHRZonesPercentage(23, 'm', HeartRateExtractor(data)))
 /* Should return {
   z1: '55.27',
   z2: '33.97',
   z3: '9.52', 
   z4: '1.11', 
-  z5: '0.13', 
-  z6: '0.00', 
-  z7: '0.00'  
+  z5a: '0.13', 
+  z5b: '0.00', 
+  z5c: '0.00'  
 } */
 ```
 ***
+
+> At this time the only devices compatible are Decathlon HR Dual Band and systems that use the same data names.
+
+> And only cycling and running are available as sports.
--- index.js
+++ index.js
@@ -9,20 +9,6 @@ exports.TheoricalMaxHeartRate = (age, gender) => {
     }
 }
 
-exports.FTPZones = (age, gender) => {
-    let maxHeartRate = this.TheoricalMaxHeartRate(age, gender)
-    let zones = {
-        "zone1": (maxHeartRate * 0.81).toFixed(0),
-        "zone2": (maxHeartRate * 0.89).toFixed(0),
-        "zone3": (maxHeartRate * 0.93).toFixed(0),
-        "zone4": (maxHeartRate * 0.99).toFixed(0),
-        "zone5": (maxHeartRate * 1.02).toFixed(0),
-        "zone6": (maxHeartRate * 1.06).toFixed(0),
-        "zone7": (maxHeartRate * 1.2).toFixed(0),
-    }
-    return zones
-}
-
 exports.HeartRateExtractor = (file) => {
     var convert = require('xml-js');
     let oneLine = ''
@@ -41,38 +27,87 @@ exports.HeartRateExtractor = (file) => {
     }
 }
 
-exports.FTPZone = (age, gender, heartRate) => {
+exports.FTHRZones = (age, gender, sport) => {
     let maxHeartRate = this.TheoricalMaxHeartRate(age, gender)
-    if (heartRate < (maxHeartRate * 0.81)) {
-        return 'zone1'
-    } else if ((heartRate >= (maxHeartRate * 0.81)) && (heartRate <= (maxHeartRate * 0.89))) {
-        return 'zone2'
-    } else if ((heartRate > (maxHeartRate * 0.89)) && (heartRate <= (maxHeartRate * 0.93))) {
-        return 'zone3'
-    } else if ((heartRate > (maxHeartRate * 0.93)) && (heartRate <= (maxHeartRate * 0.99))) {
-        return 'zone4'
-    } else if ((heartRate > (maxHeartRate * 0.99)) && (heartRate <= (maxHeartRate * 1.02))) {
-        return 'zone5'
-    } else if ((heartRate > (maxHeartRate * 1.02)) && (heartRate <= (maxHeartRate * 1.06))) {
-        return 'zone6'
-    } else if ((heartRate > (maxHeartRate * 1.06))) {
-        return 'zone7'
+    if (String(sport).toLocaleLowerCase() === 'c') {
+        return {
+            "zone1": (maxHeartRate * 0.81).toFixed(0),
+            "zone2": (maxHeartRate * 0.89).toFixed(0),
+            "zone3": (maxHeartRate * 0.93).toFixed(0),
+            "zone4": (maxHeartRate * 0.99).toFixed(0),
+            "zone5a": (maxHeartRate * 1.02).toFixed(0),
+            "zone5b": (maxHeartRate * 1.06).toFixed(0),
+            "zone5c": (maxHeartRate * 1.2).toFixed(0),
+        }
+    } else if (String(sport).toLocaleLowerCase() === 'r') {
+        return {
+            "zone1": (maxHeartRate * 0.85).toFixed(0),
+            "zone2": (maxHeartRate * 0.89).toFixed(0),
+            "zone3": (maxHeartRate * 0.94).toFixed(0),
+            "zone4": (maxHeartRate * 0.99).toFixed(0),
+            "zone5a": (maxHeartRate * 1.02).toFixed(0),
+            "zone5b": (maxHeartRate * 1.06).toFixed(0),
+            "zone5c": (maxHeartRate * 1.2).toFixed(0),
+        }
+    } else {
+        console.log('Error: Sport not found.')
+        console.log('Sports available are [c] for cycling and [r] for running.')
     }
 }
 
-exports.FTPZonesPercentage = (age, gender, arr) => {
+exports.FTHRZone = (age, gender, heartRate, sport) => {
+    let maxHeartRate = this.TheoricalMaxHeartRate(age, gender)
+    if (String(sport).toLocaleLowerCase() === 'c') {
+        if (heartRate < (maxHeartRate * 0.81)) {
+            return 'zone1'
+        } else if ((heartRate >= (maxHeartRate * 0.81)) && (heartRate <= (maxHeartRate * 0.89))) {
+            return 'zone2'
+        } else if ((heartRate > (maxHeartRate * 0.89)) && (heartRate <= (maxHeartRate * 0.93))) {
+            return 'zone3'
+        } else if ((heartRate > (maxHeartRate * 0.93)) && (heartRate <= (maxHeartRate * 0.99))) {
+            return 'zone4'
+        } else if ((heartRate > (maxHeartRate * 0.99)) && (heartRate <= (maxHeartRate * 1.02))) {
+            return 'zone5a'
+        } else if ((heartRate > (maxHeartRate * 1.02)) && (heartRate <= (maxHeartRate * 1.06))) {
+            return 'zone5b'
+        } else if ((heartRate > (maxHeartRate * 1.06))) {
+            return 'zone5c'
+        }
+    } else if (String(sport).toLocaleLowerCase() === 'r') {
+        if (heartRate < (maxHeartRate * 0.85)) {
+            return 'zone1'
+        } else if ((heartRate >= (maxHeartRate * 0.85)) && (heartRate <= (maxHeartRate * 0.89))) {
+            return 'zone2'
+        } else if ((heartRate > (maxHeartRate * 0.89)) && (heartRate <= (maxHeartRate * 0.94))) {
+            return 'zone3'
+        } else if ((heartRate > (maxHeartRate * 0.94)) && (heartRate <= (maxHeartRate * 0.99))) {
+            return 'zone4'
+        } else if ((heartRate > (maxHeartRate * 0.99)) && (heartRate <= (maxHeartRate * 1.02))) {
+            return 'zone5a'
+        } else if ((heartRate > (maxHeartRate * 1.02)) && (heartRate <= (maxHeartRate * 1.06))) {
+            return 'zone5b'
+        } else if ((heartRate > (maxHeartRate * 1.06))) {
+            return 'zone5c'
+        }
+    } else {
+        console.log('Error: Sport not found.')
+        console.log('Sports available are [c] for cycling and [r] for running.')
+    }
+}
+
+exports.FTHRZonesPercentage = (age, gender, sport, arr) => {
     let zones = {
         z1: 0,
         z2: 0,
         z3: 0,
         z4: 0,
-        z5: 0,
-        z6: 0,
-        z7: 0
+        z5a: 0,
+        z5b: 0,
+        z5c: 0
     }
     let count = 0
     arr.forEach(elem => {
-        zone = this.FTPZone(age, gender, elem)
+        zone = this.FTHRZone(age, gender, elem, sport)
         switch (zone) {
             case 'zone1':
                 zones.z1 = zones.z1 + 1
@@ -90,16 +125,16 @@ exports.FTPZonesPercentage = (age, gender, arr) => {
                 zones.z4 = zones.z4 + 1
                 count++
                 break;
-            case 'zone5':
-                zones.z5 = zones.z5 + 1
+            case 'zone5a':
+                zones.z5a = zones.z5a + 1
                 count++
                 break;
-            case 'zone6':
-                zones.z6 = zones.z6 + 1
+            case 'zone5b':
+                zones.z5b = zones.z5b + 1
                 count++
                 break;
-            case 'zone7':
-                zones.z7 = zones.z7 + 1
+            case 'zone5c':
+                zones.z5c = zones.z5c + 1
                 count++
                 break;
             default:
@@ -113,8 +148,8 @@ exports.FTPZonesPercentage = (age, gender, arr) => {
         z2: ((zones.z2 / count) * 100).toFixed(2),
         z3: ((zones.z3 / count) * 100).toFixed(2),
         z4: ((zones.z4 / count) * 100).toFixed(2),
-        z5: ((zones.z5 / count) * 100).toFixed(2),
-        z6: ((zones.z6 / count) * 100).toFixed(2),
-        z7: ((zones.z7 / count) * 100).toFixed(2)
+        z5a: ((zones.z5a / count) * 100).toFixed(2),
+        z5b: ((zones.z5b / count) * 100).toFixed(2),
+        z5c: ((zones.z5c / count) * 100).toFixed(2)
     }
 }
