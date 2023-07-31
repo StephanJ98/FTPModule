ae9e12f4a2a83ca0ac297d13b8268e7256bdb267
StephanJ98 <diego.jeandon@gmail.com>
2023-07-31 15:24:32 +0200

    :zap: TypeScript support

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-exports.TheoricalMaxHeartRate = (age, gender) => {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SpeedExtractor = exports.LTHRZonesPercentage = exports.LTHRZone = exports.LTHRZones = exports.HeartRateExtractor = exports.TheoricalMaxHeartRate = void 0;
+var TheoricalMaxHeartRate = function (age, gender) {
     switch (String(gender).toLocaleLowerCase()) {
         case 'm':
-            return 220 - age
+            return 220 - age;
         case 'f':
-            return 226 - age
+            return 226 - age;
         default:
-            return 223 - age
+            return 223 - age;
     }
-}
-
-exports.HeartRateExtractor = (file) => {
+};
+exports.TheoricalMaxHeartRate = TheoricalMaxHeartRate;
+var HeartRateExtractor = function (file) {
     try {
-        const fitDecoder = require('fit-decoder')
-        const data = (file).buffer
-        const json = fitDecoder.parseRecords(fitDecoder.fit2json(data))
-        const hrArray = fitDecoder.getRecordFieldValue(json, 'record', 'heart_rate')
-
-        let arr = []
-        hrArray.forEach(elem => {
-            if (elem != undefined) arr.push(elem)
-        })
-
-        return arr
-    } catch (error) {
-        console.log(error)
+        var fitDecoder = require('fit-decoder');
+        var data = (file).buffer;
+        var json = fitDecoder.parseRecords(fitDecoder.fit2json(data));
+        var hrArray = fitDecoder.getRecordFieldValue(json, 'record', 'heart_rate');
+        var arr_1 = [];
+        hrArray.forEach(function (elem) {
+            if (elem != undefined)
+                arr_1.push(elem);
+        });
+        return arr_1;
     }
-}
-
-exports.LTHRZones = (age, gender, sport) => {
-    let maxHeartRate = this.TheoricalMaxHeartRate(age, gender)
+    catch (error) {
+        console.log(error);
+    }
+};
+exports.HeartRateExtractor = HeartRateExtractor;
+var LTHRZones = function (age, gender, sport) {
+    var maxHeartRate = (0, exports.TheoricalMaxHeartRate)(age, gender);
     if (String(sport).toLocaleLowerCase() === 'c') {
         return {
             "zone1": (maxHeartRate * 0.56).toFixed(0),
@@ -36,127 +39,127 @@ exports.LTHRZones = (age, gender, sport) => {
             "zone3": (maxHeartRate * 0.75).toFixed(0),
             "zone4": (maxHeartRate * 0.85).toFixed(0),
             "zone5": (maxHeartRate).toFixed(0)
-        }
-    } else if (String(sport).toLocaleLowerCase() === 'r') {
+        };
+    }
+    else if (String(sport).toLocaleLowerCase() === 'r') {
         return {
             "zone1": (maxHeartRate * 0.58).toFixed(0),
             "zone2": (maxHeartRate * 0.68).toFixed(0),
             "zone3": (maxHeartRate * 0.77).toFixed(0),
             "zone4": (maxHeartRate * 0.87).toFixed(0),
             "zone5": (maxHeartRate).toFixed(0)
-        }
-    } else {
-        console.log('Error: Sport not found.')
-        console.log('Sports available are [c] for cycling or [r] for running.')
+        };
     }
-}
-
-exports.LTHRZone = (age, gender, heartRate, sport) => {
-    let maxHeartRate = this.TheoricalMaxHeartRate(age, gender)
+    else {
+        console.log('Error: Sport not found.');
+        console.log('Sports available are [c] for cycling or [r] for running.');
+    }
+};
+exports.LTHRZones = LTHRZones;
+var LTHRZone = function (age, gender, heartRate, sport) {
+    var maxHeartRate = (0, exports.TheoricalMaxHeartRate)(age, gender);
     if (String(sport).toLocaleLowerCase() === 'c') {
         if (heartRate < (maxHeartRate * 0.56)) {
-            return 'zone1'
-        } else if ((heartRate >= (maxHeartRate * 0.56)) && (heartRate <= (maxHeartRate * 0.66))) {
-            return 'zone2'
-        } else if ((heartRate > (maxHeartRate * 0.66)) && (heartRate <= (maxHeartRate * 0.75))) {
-            return 'zone3'
-        } else if ((heartRate > (maxHeartRate * 0.75)) && (heartRate <= (maxHeartRate * 0.85))) {
-            return 'zone4'
-        } else if (heartRate > (maxHeartRate * 0.85)) {
-            return 'zone5'
+            return 'zone1';
         }
-    } else if (String(sport).toLocaleLowerCase() === 'r') {
-        if (heartRate < (maxHeartRate * 0.56)) {
-            return 'zone1'
-        } else if ((heartRate >= (maxHeartRate * 0.58)) && (heartRate <= (maxHeartRate * 0.68))) {
-            return 'zone2'
-        } else if ((heartRate > (maxHeartRate * 0.68)) && (heartRate <= (maxHeartRate * 0.77))) {
-            return 'zone3'
-        } else if ((heartRate > (maxHeartRate * 0.77)) && (heartRate <= (maxHeartRate * 0.87))) {
-            return 'zone4'
-        } else if (heartRate > (maxHeartRate * 0.87)) {
-            return 'zone5'
+        else if ((heartRate >= (maxHeartRate * 0.56)) && (heartRate <= (maxHeartRate * 0.66))) {
+            return 'zone2';
         }
-    } else {
-        console.log('Error: Sport not found.')
-        console.log('Sports available are [c] for cycling and [r] for running.')
+        else if ((heartRate > (maxHeartRate * 0.66)) && (heartRate <= (maxHeartRate * 0.75))) {
+            return 'zone3';
+        }
+        else if ((heartRate > (maxHeartRate * 0.75)) && (heartRate <= (maxHeartRate * 0.85))) {
+            return 'zone4';
+        }
+        else if (heartRate > (maxHeartRate * 0.85)) {
+            return 'zone5';
+        }
     }
-}
-
-exports.LTHRZonesPercentage = (age, gender, sport, arr) => {
-    let zones = {
+    else if (String(sport).toLocaleLowerCase() === 'r') {
+        if (heartRate < (maxHeartRate * 0.56)) {
+            return 'zone1';
+        }
+        else if ((heartRate >= (maxHeartRate * 0.58)) && (heartRate <= (maxHeartRate * 0.68))) {
+            return 'zone2';
+        }
+        else if ((heartRate > (maxHeartRate * 0.68)) && (heartRate <= (maxHeartRate * 0.77))) {
+            return 'zone3';
+        }
+        else if ((heartRate > (maxHeartRate * 0.77)) && (heartRate <= (maxHeartRate * 0.87))) {
+            return 'zone4';
+        }
+        else if (heartRate > (maxHeartRate * 0.87)) {
+            return 'zone5';
+        }
+    }
+    else {
+        console.log('Error: Sport not found.');
+        console.log('Sports available are [c] for cycling and [r] for running.');
+    }
+};
+exports.LTHRZone = LTHRZone;
+var LTHRZonesPercentage = function (age, gender, sport, arr) {
+    var zones = {
         z1: 0,
         z2: 0,
         z3: 0,
         z4: 0,
         z5: 0
-    }
-    let count = 0
-    arr.forEach(elem => {
-        let zone = this.LTHRZone(age, gender, elem, sport)
+    };
+    var count = 0;
+    arr.forEach(function (elem) {
+        var zone = (0, exports.LTHRZone)(age, gender, elem, sport);
         switch (zone) {
             case 'zone1':
-                zones.z1 = zones.z1 + 1
-                count++
+                zones.z1 = zones.z1 + 1;
+                count++;
                 break;
             case 'zone2':
-                zones.z2 = zones.z2 + 1
-                count++
+                zones.z2 = zones.z2 + 1;
+                count++;
                 break;
             case 'zone3':
-                zones.z3 = zones.z3 + 1
-                count++
+                zones.z3 = zones.z3 + 1;
+                count++;
                 break;
             case 'zone4':
-                zones.z4 = zones.z4 + 1
-                count++
+                zones.z4 = zones.z4 + 1;
+                count++;
                 break;
             case 'zone5':
-                zones.z5 = zones.z5 + 1
-                count++
+                zones.z5 = zones.z5 + 1;
+                count++;
                 break;
             default:
-                console.log('Error: Key not found.')
+                console.log('Error: Key not found.');
                 break;
         }
-    })
-
+    });
     return {
         z1: ((zones.z1 / count) * 100).toFixed(2),
         z2: ((zones.z2 / count) * 100).toFixed(2),
         z3: ((zones.z3 / count) * 100).toFixed(2),
         z4: ((zones.z4 / count) * 100).toFixed(2),
-        z5a: ((zones.z5 / count) * 100).toFixed(2)
-    }
-}
-
-
-exports.SpeedExtractor = (file) => {
+        z5: ((zones.z5 / count) * 100).toFixed(2)
+    };
+};
+exports.LTHRZonesPercentage = LTHRZonesPercentage;
+var SpeedExtractor = function (file) {
     try {
-        const fitDecoder = require('fit-decoder')
-        const data = file.buffer
-        const json = fitDecoder.parseRecords(fitDecoder.fit2json(data))
-        const array = fitDecoder.getRecordFieldValue(json, 'record', 'speed')
-
-        let arr = []
-        array.forEach(elem => {
-            if (elem != undefined) arr.push(Number((elem * 3.87).toFixed(1)))
-        })
-
-        return arr
-    } catch (error) {
-        console.log(error)
+        var fitDecoder = require('fit-decoder');
+        var data = file.buffer;
+        var json = fitDecoder.parseRecords(fitDecoder.fit2json(data));
+        var array = fitDecoder.getRecordFieldValue(json, 'record', 'speed');
+        var arr_2 = [];
+        array.forEach(function (elem) {
+            if (elem !== undefined)
+                arr_2.push(Number((elem * 3.87).toFixed(1)));
+        });
+        return arr_2;
     }
-}
-
-
-/** Data that can be extracted
- *
- *  position_lat: 43.15689,-8.672544
-    position_long: -8.672544,
-    enhanced_altitude: 36.89,
-    speed: 0.085,
-    gps_accuracy: 0.03,
-    timestamp: 2022-05-19T14:29:52.000Z,
-    heart_rate: 150
- */
+    catch (error) {
+        console.log(error);
+        return [];
+    }
+};
+exports.SpeedExtractor = SpeedExtractor;
